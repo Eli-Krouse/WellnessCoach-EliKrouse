d433a787801d59dd4de2f7cf8a315520df47b4ad
Eli-Krouse <williamelijahkrouse@gmail.com>
2026-02-18 19:37:42 -0500

    Initial Commit

---
+++ WellnessCoach-EliKrouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WellnessCoach-EliKrouse
+//
+//  Created by Krouse, William E. on 2/18/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
